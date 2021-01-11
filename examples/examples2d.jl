d7de72d607ae4a59b4c0e6002bb7999cd39aff56
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2021-01-11 13:07:14 +0100

    Start with 2D example

---
+++ examples/examples2d.jl
@@ -0,0 +1,71 @@
+using PDELib
+
+
+function laplacecirc(;Plotter=nothing,nref=0)
+    
+    function circle!(builder, center, radius; n=20*2^nref)
+        points=[point!(builder, center[1]+radius*sin(t),center[2]+radius*cos(t)) for t in range(0,2π,length=n)]
+        for i=1:n-1
+            facet!(builder,points[i],points[i+1])
+        end
+        facet!(builder,points[end],points[1])
+    end
+
+    builder=SimplexGridBuilder(dim=2)
+    cellregion!(builder,1)
+    maxvolume!(builder,0.1)
+    regionpoint!(builder,0,0)
+    
+    facetregion!(builder,1)
+    circle!(builder,(0,0),1)
+
+    facetregion!(builder,2)
+    circle!(builder,(-0.2,-0.2),0.2)
+    holepoint!(builder,-0.2,-0.2)
+
+    grid=simplexgrid(builder,maxvolume=0.1*4.0^(-nref))
+
+    function flux(f,u0,edge)
+        u=unknowns(edge,u0)
+        f[1]=u[1,1]-u[1,2]
+    end
+    physics=VoronoiFVM.Physics(num_species=1,flux=flux)
+
+    sys=VoronoiFVM.System(grid,physics)
+    ispec=1
+    enable_species!(sys,ispec,[1])
+    boundary_dirichlet!(sys,ispec,1,0.0)
+    boundary_dirichlet!(sys,ispec,2,1.0)
+    inival=unknowns(sys,inival=0)
+    solution=unknowns(sys)
+    solve!(solution,inival,sys)
+
+    visualizer=GridVisualizer(Plotter=Plotter,layout=(1,2),resolution=(800,400))
+    visualize!(visualizer[1,1],grid)
+    visualize!(visualizer[1,2],grid,solution[1,:])
+    reveal(visualizer)
+end
+
+
+
+
+function laplacerect(;Plotter=nothing)
+    nspecies=1 
+    ispec=1    
+    X=collect(0:0.2:1)
+    function g!(f,u0,edge)
+        u=unknowns(edge,u0)
+        f[1]=u[1,1]-u[1,2]
+    end
+    grid=simplexgrid(X,X)
+    physics=VoronoiFVM.Physics(num_species=nspecies,flux=g!)
+    sys=VoronoiFVM.System(grid,physics)
+    enable_species!(sys,ispec,[1])
+    boundary_dirichlet!(sys,ispec,1,0.0)
+    boundary_dirichlet!(sys,ispec,3,1.0)
+    inival=unknowns(sys,inival=0)
+    solution=unknowns(sys)
+    solve!(solution,inival,sys)
+    visualize(grid,solution[1,:],Plotter=Plotter)
+end
+
---
+++ src/PDELib.jl
@@ -0,0 +1,10 @@
+module PDELib
+using Reexport
+
+@reexport using VoronoiFVM
+@reexport using ExtendableGrids
+@reexport using ExtendableGrids.GridVisualize
+@reexport using ExtendableSparse
+@reexport using SimplexGridFactory
+
+end # module
